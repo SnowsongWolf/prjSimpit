82fb7f8dcb29f4c26fcfee23e3c048d27af44c72
SnowsongWolf <zack.podany@gmail.com>
2021-01-20 07:40:57 -0600

    Added DrawFPS command

--- main.cpp
+++ main.cpp
@@ -45,6 +45,7 @@ int main()
     BeginDrawing();
     ClearBackground(BLACK);
     BeginTextureMode(tgt);
+    DrawFPS(10,10);
     ClearBackground(BLACK);
     DrawTextEx(font, "MENU", (Vector2){15.0f, 106.0f}, 20, 2, (Color){118,182,216,255});
     DrawTextEx(font, "SYSTEM", (Vector2){5.0f, 131.0f}, 13, 1, (Color){118,182,216,255});
