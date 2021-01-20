dd886ecba9614d4447d40dae8036d282f97301cc
SnowsongWolf <zack.podany@gmail.com>
2021-01-20 13:05:43 -0600

    Added more of the UI.

--- main.cpp
+++ main.cpp
@@ -64,12 +64,39 @@ int main()
     ClearBackground(BLACK);
     DrawTextEx(font, "MENU", (Vector2){27.0f, 15.0f}, 30, 2, UI_LINE_LGT);
     DrawTextEx(font, "SYSTEM", (Vector2){4.0f, 52.0f}, 20, 1, UI_LINE_LGT);
+    DrawTextEx(font, "ITEMS", (Vector2){198.0f, 52.0f}, 20, 1, UI_LINE_LGT);
+    DrawTextEx(font, "USAGE", (Vector2){63.0f, 112.0f}, 20, 1, UI_LINE_LGT);
+    DrawTextEx(font, "REQUIRED", (Vector2){62.0f, 167.0f}, 18, 1, UI_LINE_LGT);
 
     DrawRectangleRoundedLines((Rectangle){6,12,114,34}, 0.2f, 0, UI_LINE_THIN, UI_BOX);
+
+    DrawLineEx((Vector2){184,72},(Vector2){184,53},UI_LINE_THIN,UI_LINE_DRK);
+    DrawLineEx((Vector2){187,50},(Vector2){184,53},UI_LINE_THIN,UI_LINE_DRK);
+    DrawLineEx((Vector2){184,50},(Vector2){360,50},UI_LINE_THIN,UI_LINE_DRK);
+    DrawLineEx((Vector2){374,61},(Vector2){360,50},UI_LINE_THIN,UI_LINE_DRK);
+    DrawLineEx((Vector2){374,61},(Vector2){374,72},UI_LINE_THIN,UI_LINE_DRK);
+
     DrawLineEx((Vector2){0,51},(Vector2){170,51},UI_LINE_THICK,UI_LINE_LGT);
     DrawLineEx((Vector2){170,51},(Vector2){184,62},UI_LINE_THICK,UI_LINE_LGT);
     DrawLineEx((Vector2){184,62},(Vector2){184,72},UI_LINE_THICK,UI_LINE_LGT);
     DrawLineEx((Vector2){0,72},(Vector2){1280,72},UI_LINE_THICK,UI_LINE_LGT);
+    DrawLineEx((Vector2){0,7},(Vector2){1280,7},UI_LINE_THIN,UI_LINE_DRK);
+
+    DrawRectangleV((Vector2){162,101},(Vector2){892,9},UI_BOX_DRK);
+    DrawRectangleV((Vector2){162,115},(Vector2){892,26},UI_BOX_DRK);
+    DrawRectangleV((Vector2){162,175},(Vector2){633,8},UI_LINE_LGT);
+    for (int x = 263; x < 974; x += 100) {
+        DrawLineEx((Vector2){x,109},(Vector2){x,147},UI_LINE_THIN,UI_BOX);
+    }
+    for (int x = 183; x < 1041; x += 20) {
+        DrawLineEx((Vector2){x,115},(Vector2){x,141},UI_LINE_THIN,UI_BOX_LGT);
+    }
+    DrawLineEx((Vector2){162,115},(Vector2){162,141},UI_LINE_THIN,UI_LINE_LGT);
+    DrawLineEx((Vector2){162,147},(Vector2){1054,147},UI_LINE_THIN,UI_BOX_DRK);
+    DrawLineEx((Vector2){162,169},(Vector2){1054,169},UI_LINE_THIN,UI_BOX_DRK);
+    DrawLineEx((Vector2){162,187},(Vector2){1054,187},UI_LINE_THIN,UI_BOX_DRK);
+    DrawLineEx((Vector2){795,169},(Vector2){795,187},UI_LINE_THICK,UI_LINE_LGT);
+
 
     EndTextureMode();
 
