0e9691b6222a1e1df1c036a53fa348c43b0778a5
SnowsongWolf <zack.podany@gmail.com>
2021-01-22 13:06:36 -0600

    Update ui.h

--- ui.h
+++ ui.h
@@ -7,8 +7,20 @@
 
 using namespace std;
 
-enum Align {A_LEFT, A_CENTER, A_RIGHT};
-enum VAlign {V_TOP, V_CENTER, V_BOTTOM};
+public struct Padding {
+  int left;
+  int right;
+  int top;
+  int bottom;
+};
 
-void textRec(Font t_font, string strText, Rectangle t_area, float t_size, float t_spacing = 0, Vector4 t_padding = (Vector4){0,0,0,0}, Align al = A_CENTER, VAlign val = V_CENTER);
-void textRec(Font t_font, string strText, Vector2 t_point, float t_size, float t_spacing = 0, Vector4 t_padding = (Vector4){0,0,0,0}, Align al = A_CENTER, VAlign val = V_CENTER);
+#define PADDING_NONE    (Padding){0,0,0,0}
+
+public enum Align {A_LEFT, A_CENTER, A_RIGHT};
+public enum VAlign {V_TOP, V_CENTER, V_BOTTOM};
+
+public void textAlign(Font t_font, string strText, Rectangle t_area, float t_size, float t_spacing = 0, Color t_color, 
+             Padding t_padding = PADDING_NONE, Align al = A_CENTER, VAlign val = V_CENTER);
+
+public void textAlign(Font t_font, string strText, Vector2 t_point, float t_size, float t_spacing = 0, Color t_color, 
+             Padding t_padding = PADDING_NONE, Align al = A_CENTER, VAlign val = V_CENTER);
