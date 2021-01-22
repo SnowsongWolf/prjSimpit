3aad1e4d33de4372d88dd4141b840472be03add8
SnowsongWolf <zack.podany@gmail.com>
2021-01-21 18:57:00 -0600

    Adjusted colors, added definition for UI_GREEN_LGT

--- colorsheet.h
+++ colorsheet.h
@@ -1,13 +1,14 @@
 // UI colors
 #define UI_BKG          (Color){0,0,0,255}
-#define UI_LINE_DRK     (Color){35,59,82,255}
+#define UI_LINE_DRK     (Color){43,80,118,255}
 #define UI_LINE         (Color){64,136,193,255}
-#define UI_LINE_LGT     (Color){216,244,251,255}
+#define UI_LINE_LGT     (Color){240,241,250,255}
 #define UI_RED_DRK      (Color){179,58,77,255}
 #define UI_RED          (Color){242,69,89,255}
 #define UI_BOX_DRK      (Color){49,98,145,255}
 #define UI_BOX          (Color){64,135,191,255}
 #define UI_BOX_LGT      (Color){113,208,253,255}
+#define UI_GREEN_LGT    (Color){0,226,232,255}
 
 #define UI_LINE_THIN    2.0f
 #define UI_LINE_THICK   4.0f
