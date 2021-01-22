acb809acd3d782ea116b9e76f814dcec209427ec
SnowsongWolf <zack.podany@gmail.com>
2021-01-21 20:49:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ int main()
     UnloadImage(atlas);
     UnloadFileData(fileData);
 
-    RenderTexture2D tgt = LoadRenderTexture(screenWidth, screenHeight);
+    //RenderTexture2D tgt = LoadRenderTexture(screenWidth, screenHeight);
     GenTextureMipmaps(&font.texture);
     //SetTextureFilter(font.texture, FILTER_BILINEAR);
     //SetTextureFilter(font2.texture, FILTER_BILINEAR);
@@ -62,64 +62,48 @@ int main()
     {
     BeginDrawing();
     ClearBackground(BLACK);
-    /*BeginTextureMode(tgt);
-    ClearBackground(BLACK);
-    DrawTextEx(font, "MENU", (Vector2){27.0f, 15.0f}, 30, 2, UI_LINE_LGT);
-    DrawTextEx(font, "SYSTEM", (Vector2){4.0f, 52.0f}, 20, 1, UI_LINE_LGT);
-    DrawTextEx(font, "ITEMS", (Vector2){198.0f, 52.0f}, 20, 1, UI_LINE_LGT);
-    DrawTextEx(font, "USAGE", (Vector2){63.0f, 112.0f}, 28, 0, UI_LINE_LGT);
-    DrawTextEx(font, "REQUIRED", (Vector2){62.0f, 167.0f}, 18, 0, UI_LINE_LGT);
-
-    DrawRectangleRoundedLines((Rectangle){6,12,114,34}, 0.2f, 0, UI_LINE_THIN, UI_BOX);
-
-    DrawLineEx((Vector2){184,72},(Vector2){184,53},UI_LINE_THIN,UI_LINE_DRK);
-    DrawLineEx((Vector2){187,50},(Vector2){184,53},UI_LINE_THIN,UI_LINE_DRK);
-    DrawLineEx((Vector2){184,50},(Vector2){360,50},UI_LINE_THIN,UI_LINE_DRK);
-    DrawLineEx((Vector2){374,61},(Vector2){360,50},UI_LINE_THIN,UI_LINE_DRK);
-    DrawLineEx((Vector2){374,61},(Vector2){374,72},UI_LINE_THIN,UI_LINE_DRK);
-
-    DrawLineEx((Vector2){0,51},(Vector2){170,51},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){170,51},(Vector2){184,62},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){184,62},(Vector2){184,72},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){0,72},(Vector2){1280,72},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){0,7},(Vector2){1280,7},UI_LINE_THIN,UI_LINE_DRK);
-
-    DrawRectangleV((Vector2){162,101},(Vector2){892,9},UI_BOX_DRK);
-    DrawRectangleV((Vector2){162,115},(Vector2){668,26},UI_BOX_DRK);
-    DrawRectangleV((Vector2){162,175},(Vector2){633,8},UI_LINE_LGT);
-    for (int x = 263; x < 974; x += 100) {
-        DrawLineEx((Vector2){x,109},(Vector2){x,147},UI_LINE_THIN,UI_BOX);
-    }
-    for (int x = 183; x < 1041; x += 20) {
-        DrawLineEx((Vector2){x,115},(Vector2){x,141},UI_LINE_THIN,UI_BOX_LGT);
-    }
-    DrawLineEx((Vector2){162,115},(Vector2){162,141},UI_LINE_THIN,UI_LINE_LGT);
-    DrawLineEx((Vector2){162,147},(Vector2){1054,147},UI_LINE_THIN,UI_BOX_DRK);
-    DrawLineEx((Vector2){162,169},(Vector2){1054,169},UI_LINE_THIN,UI_BOX_DRK);
-    DrawLineEx((Vector2){162,187},(Vector2){1054,187},UI_LINE_THIN,UI_BOX_DRK);
-    DrawLineEx((Vector2){795,169},(Vector2){795,187},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){162+668,115},(Vector2){162+668,115+26},UI_LINE_THICK,UI_LINE_LGT);
-    DrawLineEx((Vector2){1054,101-16},(Vector2){1054,187},UI_LINE_THICK,UI_LINE_LGT);
-
-    DrawLineEx((Vector2){162,151},(Vector2){162,163},UI_LINE_THIN,UI_RED_DRK);
-    DrawLineEx((Vector2){162,163},(Vector2){1054,163},UI_LINE_THIN,UI_RED_DRK);
-    DrawLineEx((Vector2){791,151},(Vector2){791,163},UI_LINE_THICK,UI_RED);
-    DrawLineEx((Vector2){791,157},(Vector2){810,151},UI_LINE_THICK,UI_RED);
-    DrawLineEx((Vector2){791,157},(Vector2){810,163},UI_LINE_THICK,UI_RED);
-    DrawLineEx((Vector2){810,151},(Vector2){810,163},UI_LINE_THICK,UI_RED);
-
-    DrawTextEx(font, "0.76", (Vector2){848.0f, 110.0f}, 36, 0, UI_LINE_LGT);
-
-    //DrawRectangleRoundedLines((Rectangle){200,200,100,100}, 0.5f, 0, UI_LINE_THIN, UI_RED);
-    DrawRectanglePro((Rectangle){400,400,100,200}, (Vector2){0,0},0.0f,UI_LINE_LGT);
-    DrawRectanglePro((Rectangle){400,400,100,200}, (Vector2){50,100},30.0f,UI_GREEN_LGT);*/
+    //BeginTextureMode(tgt);
     
-    DrawRectangleLines(99,99,602,602,UI_LINE);
+    DrawRectangleLines(99,99,602,602,UI_LINE);      // Display area
+        
+    // MFD top row buttons
+    DrawRectangleRoundedLines((Rectangle){184,18,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){277,18,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){370,18,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){464,18,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){556,18,59,59},0.1f,0,1,UI_BOX_LGT);
+    
+    //MFD left row buttons
+    DrawRectangleRoundedLines((Rectangle){63,200,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){63,285,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){63,371,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){63,456,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){63,542,59,59},0.1f,0,1,UI_BOX_LGT);
+        
+    //MFD bottom column buttons
+    DrawRectangleRoundedLines((Rectangle){184,723,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){277,723,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){370,723,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){464,723,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){556,723,59,59},0.1f,0,1,UI_BOX_LGT);
+        
+    //MFD right column buttons
+    DrawRectangleRoundedLines((Rectangle){722,200,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,285,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,371,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,456,59,59},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,542,59,59},0.1f,0,1,UI_BOX_LGT);
+        
+    //MFD corner rocker buttons
+    DrawRectangleRoundedLines((Rectangle){63,78,59,100},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,78,59,100},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){63,613,59,100},0.1f,0,1,UI_BOX_LGT);
+    DrawRectangleRoundedLines((Rectangle){722,613,59,100},0.1f,0,1,UI_BOX_LGT);
 
-    EndTextureMode();
+    //EndTextureMode();
 
     //BeginShaderMode(sh);
-    DrawTextureRec(tgt.texture, (Rectangle){0, 0, tgt.texture.width, -tgt.texture.height}, (Vector2){0, 0}, WHITE);
+    //DrawTextureRec(tgt.texture, (Rectangle){0, 0, tgt.texture.width, -tgt.texture.height}, (Vector2){0, 0}, WHITE);
     //EndShaderMode();
     //DrawFPS(10,10);
 
@@ -127,7 +111,7 @@ int main()
     }
     TakeScreenshot("./RecTest.png");
     //sleep(10);
-    UnloadRenderTexture(tgt);
+    //UnloadRenderTexture(tgt);
 
     CloseWindow();
 
